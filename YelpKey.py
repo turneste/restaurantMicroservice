105c98f4f9e4df1e23c8d8b01a9be627c99c3e46
Steven Turner <71907094+turneste@users.noreply.github.com>
2021-10-25 17:23:14 -0400

    revise output

---
+++ YelpKey.py
@@ -0,0 +1,2 @@
+def get_key():
+    return "iCQXelQA7Dp8cMzWlVJcXB7uKMIhRDprjSmV4ujnLVRpzB2l5yjEqqJULhcKnWKs_te1xT9W_WUWc6omnZQWkK0NLQBqdUpY29BnWoWfmduecgdhotFmO93zFcF1YXYx"
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-import json, urllib.request
+import json, requests, urllib.request
+from YelpKey import get_key
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,26 +12,53 @@ def search():
     city = request.args["city"]
 
 
-    url = "https://api.yelp.com/v3/businesses/search?location=" + city
-    data = urllib.request.urlopen(url).read()
-    rest_list = json.loads(data)
-    
-    print(rest_list)
+    key = get_key()
 
-    """
-    restaurants = dict()
+    url = "https://api.yelp.com/v3/businesses/search"
+    header = {'Authorization': 'bearer %s' % key}
+    params = {
+        'location': city
+    }
 
-    
-    for i in range(1, 11):
-        restaurants[str(i)] = {
-            "name" : city,
-            "vegetarian" : False
-        }
-    
-    data = json.dumps(restaurants)
+    response = requests.get(url=url, params=params, headers=header)
 
-    """
-    return data
+    data = response.json()
+    print(data)
+
+    payload = dict()
+
+    count = 1
+    rest_num = 1
+    for restaurant in data["businesses"]:
+
+        rest_data = dict()
+
+        name = data["businesses"][rest_num]["name"]
+        try:
+            price = data["businesses"][rest_num]["price"]
+        except:
+            price = False
+        try:
+            road = data["businesses"][rest_num]["location"]["address1"]
+            city = data["businesses"][rest_num]["location"]["city"]
+            state = data["businesses"][rest_num]["location"]["state"]
+            address = [road, city, state]
+        except:
+            address = False
+
+        if price is not False and address is not False:
+            rest_data["name"] = name
+            rest_data["price"] = price
+            rest_data["address"] = address
+            count += 1
+
+            payload[str(rest_num)] = rest_data
+
+        rest_num += 1
+        if count > 10:
+            break
+
+    return json.dumps(payload)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9797)
+    app.run(debug=True)
